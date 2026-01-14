aad4e15b24f0ad6404fe6ca3670e7d0e03a08877
benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
2026-01-14 13:26:32 -0500

    HPC environment setup

---
+++ modules.sh
@@ -0,0 +1,5 @@
+#module load Python/3.12.3-GCCcore-13.3.0
+#module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
+module load Python/3.10.8-GCCcore-12.2.0
+module load CUDA/12.8.0
+
---
+++ train.sh
@@ -0,0 +1,13 @@
+export PMI_SIZE=1
+export NCCL_P2P_DISABLE=1
+accelerate launch --main_process_port 29501 src/train_cyclegan_turbo.py \
+    --pretrained_model_name_or_path="stabilityai/sd-turbo" \
+    --output_dir="output/cyclegan_turbo/uhaze" \
+    --dataset_folder "data/uhaze" \
+    --train_img_prep "resize_286_randomcrop_256x256_hflip" --val_img_prep "resize_256" \
+    --learning_rate="1e-5" --max_train_steps=25000 \
+    --train_batch_size=1 --gradient_accumulation_steps=1 \
+    --report_to "wandb" --tracker_project_name "gparmar_unpaired_h2z_cycle_debug_v2" \
+    --enable_xformers_memory_efficient_attention --validation_steps 250 \
+    --lambda_gan 0.5 --lambda_idt 1 --lambda_cycle 1
+
