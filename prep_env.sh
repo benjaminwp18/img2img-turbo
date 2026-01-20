5fce3c738b28a427eaa21f8185e1bba54ca4ce1b
benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
2026-01-20 10:58:18 -0500

    Process videos

--- prep_env.sh
+++ prep_env.sh
@@ -2,4 +2,4 @@
 #module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 module load Python/3.10.8-GCCcore-12.2.0
 module load CUDA/12.8.0
-
+. /home/bwp18/img2img-turbo/venv/bin/activate
--- src/inference_unpaired.py
+++ src/inference_unpaired.py
@@ -1,15 +1,19 @@
 import os
 import argparse
 from PIL import Image
+import cv2
 import torch
 from torchvision import transforms
 from cyclegan_turbo import CycleGAN_Turbo
 from my_utils.training_utils import build_transform
+import numpy as np
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_image', type=str, required=True, help='path to the input image')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--input_image', type=str, help='path to the input image')
+    group.add_argument('--input_video', type=str, help='path to the input video')
     parser.add_argument('--prompt', type=str, required=False, help='the prompt to be used. It is required when loading a custom model_path.')
     parser.add_argument('--model_name', type=str, default=None, help='name of the pretrained model to be used')
     parser.add_argument('--model_path', type=str, default=None, help='path to a local model state dict to be used')
@@ -39,20 +43,60 @@ if __name__ == "__main__":
 
     T_val = build_transform(args.image_prep)
 
-    input_image = Image.open(args.input_image).convert('RGB')
-    # translate the image
-    with torch.no_grad():
-        input_img = T_val(input_image)
-        x_t = transforms.ToTensor()(input_img)
-        x_t = transforms.Normalize([0.5], [0.5])(x_t).unsqueeze(0).cuda()
-        if args.use_fp16:
-            x_t = x_t.half()
-        output = model(x_t, direction=args.direction, caption=args.prompt)
+    if args.input_image is not None:
+        input_image = Image.open(args.input_image).convert('RGB')
+        # translate the image
+        with torch.no_grad():
+            input_img = T_val(input_image)
+            x_t = transforms.ToTensor()(input_img)
+            x_t = transforms.Normalize([0.5], [0.5])(x_t).unsqueeze(0).cuda()
+            if args.use_fp16:
+                x_t = x_t.half()
+            output = model(x_t, direction=args.direction, caption=args.prompt)
 
-    output_pil = transforms.ToPILImage()(output[0].cpu() * 0.5 + 0.5)
-    output_pil = output_pil.resize((input_image.width, input_image.height), Image.LANCZOS)
+        output_pil = transforms.ToPILImage()(output[0].cpu() * 0.5 + 0.5)
+        output_pil = output_pil.resize((input_image.width, input_image.height), Image.LANCZOS)
 
-    # save the output image
-    bname = os.path.basename(args.input_image)
-    os.makedirs(args.output_dir, exist_ok=True)
-    output_pil.save(os.path.join(args.output_dir, bname))
+        # save the output image
+        bname = os.path.basename(args.input_image)
+        os.makedirs(args.output_dir, exist_ok=True)
+        output_pil.save(os.path.join(args.output_dir, bname))
+    else:
+        video_cap = cv2.VideoCapture(args.input_video)
+        fps = video_cap.get(cv2.CAP_PROP_FPS)
+        total_frames = int(video_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        width = int(video_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(video_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        fourcc = cv2.VideoWriter.fourcc(*'XVID')
+        os.makedirs(args.output_dir, exist_ok=True)
+        bname = os.path.basename(args.input_video)
+        video_writer = cv2.VideoWriter(os.path.join(args.output_dir, bname),
+                              fourcc, fps, (width, height))
+
+        with torch.no_grad():
+            current_frame = 0
+            success = True
+
+            while success:
+                success, frame = video_cap.read()
+                if success:
+                    print(f'Processing frame {current_frame}/{total_frames}')
+
+                    input_img = T_val(frame)
+                    x_t = transforms.ToTensor()(input_img)
+                    x_t = transforms.Normalize([0.5], [0.5])(x_t).unsqueeze(0).cuda()
+                    if args.use_fp16:
+                        x_t = x_t.half()
+                    output_frame = model(x_t, direction=args.direction, caption=args.prompt)
+
+                    output_pil = transforms.ToPILImage()(output_frame[0].cpu() * 0.5 + 0.5)
+                    output_pil = output_pil.resize((frame.width, frame.height), Image.LANCZOS)
+                    output_cv = np.array(output_pil)
+                    output_cv = output_cv[:, :, ::-1].copy()  # Convert RGB to BGR
+                    video_writer.write(output_cv)
+
+                    current_frame += 1
+
+        video_cap.release()
+        video_writer.release()
