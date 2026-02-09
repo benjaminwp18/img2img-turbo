1d6af6ac51f294c4a21939cf5934d472c8f7df2c
benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
2026-02-09 17:49:45 -0500

    Training from checkpoint fails

--- train_paired.sh
+++ train_paired.sh
@@ -1,5 +1,5 @@
 accelerate launch src/train_pix2pix_turbo.py \
-    --pretrained_model_name_or_path="/home/bwp18/img2img-turbo/output/pix2pix_turbo/phaze/checkpoints/model_2501.pkl" \
+    --pretrained_model_name_or_path="stabilityai/sd-turbo" \
     --output_dir="output/pix2pix_turbo/phaze" \
     --dataset_folder="data/phaze" \
     --resolution=512 \
@@ -9,4 +9,5 @@ accelerate launch src/train_pix2pix_turbo.py \
     --track_val_fid \
     --report_to "wandb" --tracker_project_name "pix2pix_turbo_phaze"
 
-#    --pretrained_model_name_or_path="stabilityai/sd-turbo" \
+#    --pretrained_model_name_or_path="/home/bwp18/img2img-turbo/output/pix2pix_turbo/phaze/checkpoints/model_2501.pkl" \
+
